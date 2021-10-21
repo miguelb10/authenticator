fda92f930bccbedaee14855394c430990eec1f09
miguelb10 <miguel.blas_03@hotmail.com>
2021-10-21 13:02:04 -0500

    add config heroku

--- src/main/java/com/ionix/authenticator/controller/UserRestController.java
+++ src/main/java/com/ionix/authenticator/controller/UserRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin()
+@CrossOrigin(origins = {"http://localhost:4200", "*"})
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
